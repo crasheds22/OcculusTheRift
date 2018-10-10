f07c0db29859c01d941cc495ab19e0d578bd16e2
DESKTOP-B701R7Q\Kinsley <ltkinsella91@gmail.com>
2018-10-10 20:44:45 +0800

    Added comments for SoundPlayer and Dungeon class

--- eNGin/SoundPlayer.h
+++ eNGin/SoundPlayer.h
@@ -10,14 +10,33 @@
 class SoundPlayer
 {
 public:
+	/**
+	*	Default constructor
+	*	Sets up required SDL Audio and loads sound files
+	*/
 	SoundPlayer();
 
+	/**
+	*	Default destructor
+	*	Frees sounds from memory
+	*/
 	~SoundPlayer();
 
+	/**
+	*	A normal member taking 1 argument
+	*	Called by game to play the sound at point soundId, automatically plays on an available sound channel
+	*	@param soundId the index of the sound to be played
+	*/
 	void PlaySound(int soundId);
 
 private:
-	std::vector<Mix_Chunk*> sounds;
+	std::vector<Mix_Chunk*> sounds; /*<! Vector of all sounds used in game>*/
 };
 
 #endif
+
+
+
+
+
+
