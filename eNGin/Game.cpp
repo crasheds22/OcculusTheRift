6a9fd1d1a626d1b7ec29eef1f1e5ecd12ecc3ba8
Rebecca <beckylum0214@gmail.com>
2018-10-24 16:29:39 +0800

    RL tested application on other machines, tweaked mouse sensitivity settings

--- eNGin/Game.cpp
+++ eNGin/Game.cpp
@@ -385,7 +385,7 @@ void Game::MouseLook(int x, int y)
 	int deadzone = 0.25;
 	Vector3 pitchAxis;
 	GLdouble fps = 60;
-	GLdouble mouseSensitivity = 3;
+	GLdouble mouseSensitivity = 2;
 
 	//If the mouse pointer has moved far enough, rotate camera
 	if ((abs((long double)x) > deadzone) || (abs((long double)y) > deadzone)) 
